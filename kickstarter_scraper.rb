3b4870c4bc61e894a0514b78444728ff7ed8ec00
Donovan Jones <donovanljones94@gmail.com>
2019-09-30 18:08:41 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -8,7 +8,9 @@ def create_project_hash
   projects = {}
   
   kickstarter.css("li.project.grid_4").each do |project|
-    projects[project] = {}
+    title = project.css("h2.bbcard_name strong a").text
+    projects[title.to_sym] = {}
+  end
     
   #projects: kickstarter.css("li.project.grid_4")
   #title: project.css("h2.bbcard_name strong a").text
@@ -17,8 +19,9 @@ def create_project_hash
   #location ul.project-meta span.location-name").text
   #percent funded: project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
   
+  projects 
 end
 
-projects
+
 
 create_project_hash
